13cd9f057b350f792850c9b0c0a4f5b6a4402638
BatOrgil7 <batorgilrb@gmail.com>
2026-07-30 10:38:41 +0900

    Added Gemini free tier API

--- AI_Chatbot.py
+++ AI_Chatbot.py
@@ -1,5 +1,18 @@
+import os
+from dotenv import load_dotenv
 from openai import OpenAI
+from google import genai
 
-client = OpenAI()
+load_dotenv()
 
-respone = client.responses.create()
+api_key = os.getenv("MY_OPEN_AI_API_KEY")
+
+client = OpenAI(api_key=api_key)
+client1 = genai.Client()
+
+respone = client1.models.generate_content(
+    model="gemini-3.6-flash",
+    contents='Say hello!'
+)
+
+print(respone.text)
